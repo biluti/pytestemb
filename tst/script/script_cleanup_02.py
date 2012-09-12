50b04d39d734b276dfa352ada8bcac1161201429
jmb <jm.beguinet@gmail.com>
2012-09-12 11:29:12 +0200

    new test script

---
+++ tst/script/script_cleanup_02.py
@@ -0,0 +1,51 @@
+
+
+"""
+@goal: -
+@todo: -
+@warning: -
+@requires: -
+@precondition: -
+@postcondition: -
+"""
+
+
+
+import pytestemb as test
+
+
+import time
+
+
+        
+def setup():
+    raise Exception()
+
+def cleanup():
+    raise Exception()
+
+
+def case_01():
+    test.assert_true(1==1, "1==1")
+
+
+if __name__ == "__main__":
+    
+
+    test.set_setup(setup)
+    test.add_test_case(case_01)
+    test.set_cleanup(cleanup)
+    test.run_script()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
---
+++ tst/script/script_create_01.py
@@ -0,0 +1,61 @@
+
+
+"""
+@goal: -
+@todo: -
+@warning: -
+@requires: -
+@precondition: -
+@postcondition: -
+"""
+
+
+
+import pytestemb as test
+
+
+import time
+
+
+def defaultValue():
+    """
+    @goal : -
+    @coverage : -
+    """
+    test.assert_true_fatal(1==2, "1==2")
+    test.assert_true(1==1, "1==1")
+
+        
+def start():
+    pass
+
+def stop():
+    pass
+
+
+def create():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    
+    test.set_create(create)
+    test.set_setup(start)
+    test.add_test_case(defaultValue)
+    test.set_cleanup(stop)
+    test.run_script()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
