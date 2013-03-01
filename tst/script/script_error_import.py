f55130f91608f24baa04fc9910514bf6b8b228bb
jmb <jm.beguinet@gmail.com>
2013-03-01 18:05:41 +0100

    version 1.7.1

---
+++ tst/script/script_error_import.py
@@ -0,0 +1,64 @@
+
+
+"""
+@goal: -
+@todo: -
+@warning: -
+@requires: -
+@precondition: -
+@postcondition: -
+"""
+
+
+
+
+
+import pytestemb as test
+
+
+import time
+
+
+import ttt
+
+def defaultValue():
+    """
+    @goal : -
+    @coverage : -
+    """
+    pass
+
+
+
+        
+        
+def start():
+    pass
+
+def stop():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    test.set_setup(start)
+    test.add_test_case(defaultValue)
+    test.set_cleanup(stop)
+    test.run_script()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
---
+++ tst/script/script_ueid.py
@@ -0,0 +1,62 @@
+
+
+"""
+@goal: -
+@todo: -
+@warning: -
+@requires: -
+@precondition: -
+@postcondition: -
+"""
+
+
+
+import pytestemb as test
+
+
+import time
+
+
+
+def t4():
+    """
+    @goal : -
+    @coverage : -
+    """    
+    print test.get_uedi()
+
+
+
+
+        
+        
+def start():
+    print test.get_uedi()
+
+def stop():
+    print test.get_uedi()
+
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    test.set_setup(start)
+    test.add_test_case(t4)
+    test.set_cleanup(stop)
+    test.run_script()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
