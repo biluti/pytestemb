4f2eeef4fb3c972c67a0614d48a3501d60611e90
Jean-Marc Beguinet <jean-marc.beguinet@parrot.com>
2014-09-05 18:04:43 +0200

    [DEV] debian package generation script

---
+++ gen_deb.sh
@@ -0,0 +1,34 @@
+# !/bin/bash
+
+PACKAGE_NAME="pytestemb-lib"
+
+echo "copy pytestemb source files"
+
+mkdir -p deb/usr/local/lib/python2.7/dist-packages
+rm -rf deb/usr/local/lib/python2.7/dist-packages/*
+
+
+rm -rf src/pytestemb/*.pyc
+cp -rv src/pytestemb/ deb/usr/local/lib/python2.7/dist-packages
+
+
+# write version
+version=$(grep -h "VERSION_STRING *= " src/pytestemb/__init__.py | sed s/"VERSION_STRING .*= \""// | sed s/'"'//)
+echo $version
+
+sed -i '/^Package: / c\
+Package: '${PACKAGE_NAME}'' ./deb/DEBIAN/control
+sed -i '/^PACKAGE_NAME=/ c\
+PACKAGE_NAME='${PACKAGE_NAME}'' ./deb/DEBIAN/prerm
+sed -i '/^PACKAGE_NAME=/ c\
+PACKAGE_NAME='${PACKAGE_NAME}'' ./deb/DEBIAN/postinst
+
+sed -i '/^Version: / c\
+Version: '${version}'' ./deb/DEBIAN/control
+
+
+dpkg-deb --build deb $PACKAGE_NAME-${version}.deb
+
+rm -r ./deb/usr/local/lib/python2.7/dist-packages/*
+
+echo "end"
