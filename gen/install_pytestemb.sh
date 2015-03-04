d33e0127396ecf05b809f19eb1935b1a8c4288bd
Jean-Marc Beguinet <jean-marc.beguinet@parrot.com>
2015-03-04 12:00:46 +0100

    [DEV] update install script in case of dir already exist

--- gen/install_pytestemb.sh
+++ gen/install_pytestemb.sh
@@ -3,7 +3,7 @@
 
 
 echo "install pytestemb"
-mkdir /usr/local/lib/python2.7/dist-packages/pytestemb
+mkdir -p /usr/local/lib/python2.7/dist-packages/pytestemb
 
 cd ../src/pytestemb/ 
 tar -cf - `find . -name "*.py" -print` | ( cd /usr/local/lib/python2.7/dist-packages/pytestemb && tar xBf - )
