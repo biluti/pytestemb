802fd579263046f6440eaf095d4b6ce6b70d6f1b
Jean-Marc Beguinet <jean-marc.beguinet@parrot.com>
2015-04-03 11:41:00 +0200

    [DEV] update logstash pytestemb

--- elk/logstash_pytestemb.sh
+++ elk/logstash_pytestemb.sh
@@ -1,6 +1,6 @@
 until ~/bin/logstash-1.4.2/bin/logstash -f ~/workspace/elastic/logstash_pytestemb.txt; do
     echo "#########################################################"
-    echo "'logstash' crashed with exit code $?.  Respawning.." >&1
+    echo "'logstash' exit with code $?.  Respawning.." >&1
     echo "#########################################################"
     echo "wait 30 sec"
     sleep 30
