5bdf470325e37367dffd2970bbde34307c8c80c2
Jean-Marc Beguinet <jean-marc.beguinet@parrot.com>
2017-03-03 17:04:02 +0100

    [DEV] remove elk script

--- elk/logstash_pytestemb.sh
+++
@@ -1,8 +0,0 @@
-until ~/bin/logstash-1.4.2/bin/logstash -f ~/workspace/elastic/logstash_pytestemb.txt; do
-    echo "#########################################################"
-    echo "'logstash' exit with code $?.  Respawning.." >&1
-    echo "#########################################################"
-    echo "wait 30 sec"
-    sleep 30
-    echo "retry"
-done
